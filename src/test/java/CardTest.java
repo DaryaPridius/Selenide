b681f12d91fe09c98e31a2a4521510c0d8204228
Pridius <darya.pridius@yandex.ru>
2024-08-07 16:37:45 +0300

    Initial

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+class CardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver");
+    }
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void sendingFormWithValidData(){
+        driver.get("http://localhost:9999");
+    }
+
+}
