d01f9bad62d19e37b90022357cefa6bb6a188094
Pridius <darya.pridius@yandex.ru>
2024-08-07 18:17:18 +0300

    add the first test and README.md

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,31 +1,30 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-class CardTest {
-    private WebDriver driver;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
-    @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver");
-    }
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
+public class CardTest {
+
+    @BeforeMethod
+    public void setupAll() {
+        open("http://localhost:9999/");
     }
 
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
-    }
+    SelenideElement form = $(".form");
 
     @Test
-    void sendingFormWithValidData(){
-        driver.get("http://localhost:9999");
+    public void sendingFromWithValidData() {
+        form.$("[data-test-id=name] input").setValue("Иванов Олег");
+        form.$("[data-test-id=phone] input").setValue("+79884555555");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button.button").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
 
 }
